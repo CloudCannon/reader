d64ac9d86be2ade97611a22a958d56ee67a13f63
Melissa Mepham <mel@cloudcannon.com>
2021-06-22 10:44:08 +1200

    merging ts into main

--- node_modules/ts-to-zod/lib/utils/getImportPath.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Resolve the path of an import.
- *
- * @param from path of the current file
- * @param to path of the import file
- * @returns relative path without extension
- */
-export declare function getImportPath(from: string, to: string): string;
